f3f35bd1cc96824a3ad2ea07f935c3e68eacd055
kleberkunha <kleberkunha@hotmail.com>
2021-09-06 18:36:58 +0200

    updating

--- src/pages/Apartments/index.jsx
+++ src/pages/Apartments/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Carousel from 'react-bootstrap/Carousel'
 
 const ApartmentsProfile = () => {
@@ -51,12 +52,42 @@ const ApartmentsProfile = () => {
             </Carousel>
           </div>
           <div className="col-3 contact-box border">
-            <div className="d-flex">
-              <div className="icoimg">
+            <div className="d-flex mt-4">
+              <div className="icoimg"></div>
+              <div className="text-left-contact mt-2 mx-3">
+                <p className="m-0">Casa26</p>
+                <div className="d-flex left-side-details-contact">
+                  <p className="phone-number-hide">220</p>
+                  <Link className="show-number">Show Number</Link>
+                </div>
               </div>
-              <div>
-                <p></p>
-              </div>
+            </div>
+            <div>
+              <form>
+                <div class="form-row">
+                  <div class="form-group col-md-11">
+                    <label for="inputEmail4">Name*</label>
+                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email"/>
+                  </div>
+                  <div class="form-group col-md-11">
+                    <label for="inputPassword4">Email*</label>
+                    <input type="password" class="form-control" id="inputPassword4" placeholder="Password"/>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="inputAddress">Phone Number*</label>
+                  <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
+                </div>
+                <label htmlFor="exampleFormControlTextarea1">
+                  Send a message.
+                </label>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="5"/>
+                <p className="text-responsible mt-2">Responsible for processing your data: THP NEXT</p>
+                <input class="form-check-input" type="checkbox" id="gridCheck1"/>
+                <label class="form-check-label mx-2" for="gridCheck1">
+                  Example checkbox
+                </label>
+              </form>
             </div>
           </div>
         </div>
