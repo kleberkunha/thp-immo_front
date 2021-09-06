5af8995ae92734fe3941de550f6793c7fc95e1fc
Augustin <augustin.berne.pro@gmail.com>
2021-09-06 21:51:13 +0200

    Search bar homepage done

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 const Jumbotron = () => {
   return (
-    <div className="home-header jumbotron bg-cover">
-      <section className="hero jumbotron-catchphrase py-5">
-        <div className="vertical-center row">
+    <div className="home-header jumbotron bg-cover d-flex align-items-center justify-content-center">
+      <section className="hero jumbotron-content d-flex row justify-content-center">
+        <div className="">
           <h1 className="py-3 d-flex justify-content-center">Le meilleur site pour toutes vos annonces immobilières</h1>
           <h3 className="py-3 d-flex justify-content-center">C'est gratuit, et c'est facile</h3>
+        </div>
+        <div className=" py-3 my-5 d-flex justify-content-center search-container">
+          <form className="">
+            <input type="text" className="search-input d-flex justify-content-center" />
+          </form>
         </div>
       </section>
     </div>
