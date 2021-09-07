734a9d422764ab0758261e1e781bbdd01c46e1f9
Augustin <augustin.berne.pro@gmail.com>
2021-09-07 15:56:41 +0200

    Adding cards to homepage

--- src/components/HousingList/HousingList.jsx
+++ src/components/HousingList/HousingList.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import HousingCard from 'components/HousingCard/HousingCard';
+// import HousingCard from 'components/HousingCard/HousingCard';
+import ApartmentCard from 'components/CardsApComponent/ApartmentCard';
 
 const Housinglist = () => {
   return (
-    <div className="row d-flex justify-content-around">
-        <HousingCard />
-        <HousingCard />
-        <HousingCard />
-        <HousingCard />
-        <HousingCard />
-        <HousingCard />
+    <div className="m-5">
+     <ul className="d-flex justify-content-around align-items-center flex-wrap m-5">
+     {/* <ul className="d-flex flex-wrap m-5"> */}
+      <ApartmentCard />
+      <ApartmentCard />
+      <ApartmentCard />
+      <ApartmentCard />
+      <ApartmentCard />
+      <ApartmentCard />
+      <ApartmentCard />
+      <ApartmentCard />
+      <ApartmentCard />
+      <ApartmentCard />
+      <ApartmentCard />
+      <ApartmentCard />
+      <ApartmentCard />
+      <ApartmentCard />
+      <ApartmentCard />
+     </ul>
     </div>
   );
 };
@@ -22,4 +35,15 @@ export default Housinglist;
   {housingList.map((housingData) => (
     <HousingCard data={housingData} key={housingData.id} />
   ))}
+</div> */}
+
+
+// HERE IS SOMETHING TO KEEP FOR NOW, MAY BE DELETED LATER
+{/* <div className="row d-flex justify-content-around">
+  <HousingCard />
+  <HousingCard />
+  <HousingCard />
+  <HousingCard />
+  <HousingCard />
+  <HousingCard />
 </div> */}
