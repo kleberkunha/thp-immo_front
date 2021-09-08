598ce4837cc80553889414504c40365fce9ef6c2
MathieuParadis <mathieuyl.paradis@gmail.com>
2021-09-08 09:31:39 +0100

    connected the back to the front for view properties

--- src/components/PropertyPresentation/PropertyPresentation.jsx
+++ src/components/PropertyPresentation/PropertyPresentation.jsx
@@ -1,37 +1,21 @@
 import React from 'react';
 
-const PropertyPresentation = ({description}) => {
+const PropertyPresentation = ({category, location, price, description}) => {
   return (
     <div className="property-presentation">
       <div className="property-data">
-        <h2 className="title-properties-responsive">Property</h2>
-        <div className="properties properties-responsive">
-          <div className="prop-1">
-            <div>Useful area (m²): 315 m²</div>
-            <div>Enterprise: no</div>
-            <div>Bathrooms: 4 or more</div>
-          </div>
-          <div className="prop-2">
-            <div>gross area (m²): 618 m²</div>
-            <div>Typology: T4</div>
-            <div>Condition: Used</div>
-          </div>
-          <div className="prop-3">
-            <div>land area (m²): 303 m²</div>
-            <div>Construction year: 2010</div>
-          </div>
+        <h2 className="title-properties-responsive">Détails</h2>
+        <div className="properties properties-responsive d-flex flex-column">
+          <p><strong>Type de logement :</strong> {category}</p>
+          <p><strong>Lieu :</strong> {location}</p>
+          <p><strong>Prix :</strong> {price} €/semaine</p>
         </div>
         <hr />
       </div>
       <div className="apartment-description-section">
         <section className="section-responsive">
           <h2>Description</h2>
-          <div>
-            <div>
-              <p></p>
-              {description}
-            </div>
-          </div>
+            {description}
         </section>
         <hr />
       </div>
--- src/pages/Apartment/Apartment.jsx
+++ src/pages/Apartment/Apartment.jsx
@@ -43,8 +43,6 @@ const ApartmentsProfile = () => {
   },[]);
 
   const listing = listings.listings[id];
-  const category = categories.categories[id];
-  const location = locations.locations[id];
 
   let fourListings = [];
   fourListings.push(listings.listings[0], listings.listings[1], listings.listings[2], listings.listings[3])
@@ -53,68 +51,41 @@ const ApartmentsProfile = () => {
   return (
     <div>
       {
-        listings.listings && categories.categories && locations.locations ? 
+        listing ? 
 
           <div className="container infoAp-location-price border">
             <div className="row d-flex top-content-location-price-responsive">
               <div className="row title-plus-location d-block mt-3">
                 <div className="col-sm title-responsive">
                   <h5>{listing.title}</h5>
-                  <p>{location.name}</p>
-                </div>
-              </div>
-              <div className="row d-block top-pricing mt-3">
-                <div className="col-sm total-price">
-                  <h5>{listing.price} €/semaine</h5>
-                </div>
-                <div className="col-sm price-m2">
-                  {category.name}
                 </div>
               </div>
             </div>
             <div className="container p-0 mt-2 content-carousel-contact-responsive">
               <div className="row d-flex">
                 <div className="col-9 carousel-box border">
-                  <Carousel>
-                    <Carousel.Item>
-                      <img
-                        className="d-block w-100"
-                        src={listing.photo}
-                        alt="First slide"
-                      />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                      <img
-                        className="d-block w-100"
-                        src="https://medias.residences-immobilier.com/pict/f600x400/2/5/7/6/ext_0_2576638.jpg?t=9685981"
-                        alt="Second slide"
-                      />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                      <img
-                        className="d-block w-100"
-                        src="https://www.proprietesparisiennes-sothebysrealty.com/datas/biens/thumbs/995/995_00-2020-10-21-1630.jpg"
-                        alt="Third slide"
-                      />
-                    </Carousel.Item>
-                  </Carousel>
+                  <img className="d-block w-100" src={listing.photo} alt="property img" />
                 </div>
-                <ContactCard />
+                { listing && users.user ?
+                  <ContactCard email={users.user[listing.user_id - 1].email} /> : 'loading'
+                }
+                
               </div>
             </div>
-
-            <PropertyPresentation description={listing.description}/>
-            <div className="container main-card-list-profileAp">
-            {
-              listings.listings ? <HousingList data={fourListings} /> : <Loading />
+            { listing && categories.categories && locations.locations ?
+              <PropertyPresentation category={categories.categories[listing.category_id - 1].name} location={locations.locations[listing.location_id - 1].name} price={listing.price} description={listing.description}/> : "loading"
             }
 
+            
+            <div className="container main-card-list-profileAp">
+              {
+                listings.listings ? <HousingList data={fourListings} /> : <Loading />
+              }
             </div>
           </div>
 
-  : <Loading /> 
-          }
-
+        : <Loading /> 
+      }
     </div>
   )
 }
